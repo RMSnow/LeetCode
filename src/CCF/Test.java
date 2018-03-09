eaa0ff3be3cae25d708c2109960fce0771d96577
snow <xueyao_98@foxmail.com>
2018-03-09 17:25:58 +0800

    CCF-P3 finished

--- src/CCF/Test.java
+++ src/CCF/Test.java
@@ -1,5 +1,6 @@
 package CCF;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -8,54 +9,27 @@ import java.util.Calendar;
  * Created by snow on 06/03/2018.
  */
 public class Test {
-    public static void main(String[] args) {
-//        String minutes = "*";
-//        ArrayList<Integer> minutesList = genValues(minutes, 0, 59);
+    public static void main(String[] args) throws ParseException {
+//        String start = "201711170032";
+//        String end = "201711222352";
 //
-//        //test
-//        for (int i = 0; i < minutesList.size(); i++) {
-//            System.out.println(minutesList.get(i));
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmm");
+//        Calendar now = Calendar.getInstance();
+//        for (int i = 0; i < 7; i++) {
+//            System.out.printf(now.get(now.MONTH) + 1 + "月" + now.get(now.DAY_OF_MONTH) + "号是周");
+//            System.out.println(now.get(now.DAY_OF_WEEK) - 1);
+//            now.add(Calendar.DATE, 1);
 //        }
 
-        //Calendar
+        ArrayList<Integer> a = new ArrayList<>();
+        ArrayList<Integer> b = new ArrayList<>();
+        a.add(1);
+        b.add(2);
+        b.add(3);
+        a.addAll(b);
 
-//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmm");
-//        Calendar calendar = Calendar.getInstance();
-//
-//        System.out.println(simpleDateFormat.format(calendar.getTime()));
+        for (int i=0;i<a.size();i++) System.out.println(a.get(i));
 
-        String start = "201711170032";
-        String end = "201711222352";
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmm");
-        Calendar startCalendar = Calendar.getInstance();
-        Calendar endCalendar = Calendar.getInstance();
-
-        int startYear = Integer.parseInt(start.substring(0, 4));
-        int endYear = Integer.parseInt(end.substring(0, 4));
-
-        int startMonth = parseFromTag(start, 4);
-        int endMonth = parseFromTag(end, 4);
-
-        int startDay = parseFromTag(start, 6);
-        int endDay = parseFromTag(end, 6);
-
-        int startHour = parseFromTag(start, 8);
-        int endHour = parseFromTag(end, 8);
-
-        int startMinute = parseFromTag(start, 10);
-        int endMinute = parseFromTag(end, 10);
-
-
-        System.out.println(startYear + "-" + startMonth + "-" + startDay + "-" + startHour + "-" + startMinute);
-        System.out.println(endYear + "-" + endMonth + "-" + endDay + "-" + endHour + "-" + endMinute);
-
-        startCalendar.set(startYear, startMonth - 1, startDay, startHour, startMinute);
-        endCalendar.set(endYear, endMonth - 1, endDay, endHour, endMinute);
-
-        for (; startCalendar.compareTo(endCalendar) < 0; startCalendar.add(Calendar.DATE, 1)) {
-            System.out.println(dateFormat.format(startCalendar.getTime()));
-        }
 
     }
 
--- src/CCF/Y17M12P3.java
+++ src/CCF/Y17M12P3.java
@@ -1,5 +1,6 @@
 package CCF;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -7,21 +8,6 @@ import java.util.*;
  * Created by snow on 05/03/2018.
  */
 public class Y17M12P3 {
-//    n s t
-//    yyyymmddHHMM
-
-//    3 201711170032 201711222352
-//    0 7 * * 1,3-5 get_up
-//    30 23 * * Sat,Sun go_to_bed
-//    15 12,18 * * * have_dinner
-
-//    min hour dayOfMonth month dayOfWeek command
-
-//    特殊符号：
-//    * , -
-//    * 只能单独出现
-//    , 与 - 可以同时出现
-
 
 //    其他要求：
 //    （1）数字 / 英文缩写（不区分大小写），且可以混合使用
@@ -34,35 +20,30 @@ public class Y17M12P3 {
         String start = scanner.next();
         String end = scanner.next();
 
-        //start & end
+//        int startYear = Integer.parseInt(start.substring(0, 4));
+//        int endYear = Integer.parseInt(end.substring(0, 4));
+//
+//        int startMonth = parseFromTag(start, 4);
+//        int endMonth = parseFromTag(end, 4);
+//
+//        int startDay = parseFromTag(start, 6);
+//        int endDay = parseFromTag(end, 6);
+//
+//        int startHour = parseFromTag(start, 8);
+//        int endHour = parseFromTag(end, 8);
+//
+//        int startMinute = parseFromTag(start, 10);
+//        int endMinute = parseFromTag(end, 10);
+//
+//        Calendar startCalendar = Calendar.getInstance();
+//        Calendar endCalendar = Calendar.getInstance();
+//        startCalendar.set(startYear, startMonth - 1, startDay, startHour, startMinute);
+//        endCalendar.set(endYear, endMonth - 1, endDay, endHour, endMinute);
 
-        Calendar startCalendar = Calendar.getInstance();
-        Calendar endCalendar = Calendar.getInstance();
-
-        int startYear = Integer.parseInt(start.substring(0, 4));
-        int endYear = Integer.parseInt(end.substring(0, 4));
-
-        int startMonth = parseFromTag(start, 4);
-        int endMonth = parseFromTag(end, 4);
-
-        int startDay = parseFromTag(start, 6);
-        int endDay = parseFromTag(end, 6);
-
-        int startHour = parseFromTag(start, 8);
-        int endHour = parseFromTag(end, 8);
-
-        int startMinute = parseFromTag(start, 10);
-        int endMinute = parseFromTag(end, 10);
-
-        startCalendar.set(startYear, startMonth - 1, startDay, startHour, startMinute);
-        endCalendar.set(endYear, endMonth - 1, endDay, endHour, endMinute);
-
-//        System.out.println(startYear + "-" + startMonth + "-" + startDay + "-" + startHour + "-" + startMinute);
-//        System.out.println(endYear + "-" + endMonth + "-" + endDay + "-" + endHour + "-" + endMinute);
+        //------------------- 对接下来的n行，分别解析各项 -------------------
 
         ArrayList<String> finalCommands = new ArrayList<>();
 
-        //对接下来的n行，分别解析各项
         for (int i = 0; i < n; i++) {
             String minutes = scanner.next();
             String hours = scanner.next();
@@ -71,38 +52,35 @@ public class Y17M12P3 {
             String dayOfWeek = scanner.next();
             String command = scanner.next();
 
-//            test
-//            1  201711170032 201711222352
-//            0 7 * * 1,3-5 get_up
-
-            finalCommands = genCommands(startCalendar, endCalendar,
+            ArrayList<String> tempList = genCommands(start, end,
                     minutes, hours, dayOfMonth, month, dayOfWeek, command);
-        }
-
-        //test
-        for (int i = 0; i < finalCommands.size(); i++) {
-            System.out.println(finalCommands.get(i));
+            finalCommands.addAll(tempList);
         }
 
         //将生成的n个命令集合并，排序后输出
+        Collections.sort(finalCommands);
+        for (int i = 0; i < finalCommands.size(); i++) {
+            System.out.println(finalCommands.get(i));
+        }
     }
 
-    public static ArrayList<String> genCommands(Calendar startCalendar, Calendar endCalendar,
+    public static ArrayList<String> genCommands(String start, String end,
                                                 String minutes, String hours,
                                                 String dayOfMonth, String month,
                                                 String dayOfWeek, String command) {
 
-//    特殊符号：
-//    * , -
-//    * 只能单独出现
-//    , 与 - 可以同时出现
+        //------------------- start & end -------------------
 
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmm");
+        Calendar startCalendar = Calendar.getInstance();
+        Calendar endCalendar = Calendar.getInstance();
+        try {
+            startCalendar.setTime(dateFormat.parse(start));
+            endCalendar.setTime(dateFormat.parse(end));
 
-//    其他要求：
-//    （1）数字 / 英文缩写（不区分大小写），且可以混合使用
-//    （2）若同一时刻有多个指令，则按先后顺序
-//    （3）分钟数、小时数可以有前导0，也可以没有
-
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
         //------------------- <minutes> and <hours> -------------------
         ArrayList<Integer> minutesList = genValues(minutes, 0, 59);
@@ -110,10 +88,10 @@ public class Y17M12P3 {
 
         //------------------- <day of month> and <month> -------------------
         ArrayList<Integer> dayOfMonthList = genValues(dayOfMonth, 1, 31);
-        ArrayList<Integer> monthList = genValues(month, 1, 12);
+        ArrayList<Integer> monthList = genValues(month, 1, 12, "month");
 
         //------------------- <day of week> -------------------
-        ArrayList<Integer> dayOfWeekList = genValues(dayOfWeek, 0, 6);
+        ArrayList<Integer> dayOfWeekList = genValues(dayOfWeek, 0, 6, "week");
 
 
         //TODO: 对于month与week暂时不考虑缩写
@@ -122,19 +100,14 @@ public class Y17M12P3 {
         //------------------- 检测可行性 -------------------
 
         ArrayList<String> commands = new ArrayList<>();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmm");
-
-        //????  小时的判断，在起始时间不为0时，有问题
 
         for (; startCalendar.compareTo(endCalendar) < 0; startCalendar.add(Calendar.DATE, 1)) {
 
             //首先判断"天"
 
-            System.out.println(dateFormat.format(startCalendar.getTime()));
-
-            if (!monthList.contains(startCalendar.MONTH + 1) ||
-                    !dayOfMonthList.contains(startCalendar.DAY_OF_MONTH) ||
-                    !dayOfWeekList.contains(startCalendar.DAY_OF_WEEK)) {
+            if (!monthList.contains(startCalendar.get(Calendar.MONTH) + 1) ||
+                    !dayOfMonthList.contains(startCalendar.get(Calendar.DAY_OF_MONTH)) ||
+                    !dayOfWeekList.contains(startCalendar.get(Calendar.DAY_OF_WEEK) - 1)) {
 
                 //将时间改为00:00
                 startCalendar.set(Calendar.HOUR_OF_DAY, 0);
@@ -151,9 +124,6 @@ public class Y17M12P3 {
             temp.setTime(startCalendar.getTime());
 
             Iterator hoursIt = hoursList.iterator();
-            Iterator minutesIt = minutesList.iterator();
-
-
             while (hoursIt.hasNext()) {
                 int h = (int) hoursIt.next();
                 temp.set(Calendar.HOUR_OF_DAY, h);
@@ -161,16 +131,16 @@ public class Y17M12P3 {
                 if (temp.compareTo(startCalendar) < 0)
                     continue;
 
-                while (minutesIt.hasNext()) {
-                    int m = (int) minutesIt.next();
+                for (int i = 0; i < minutesList.size(); i++) {
+                    int m = minutesList.get(i);
                     temp.set(Calendar.MINUTE, m);
 
                     if (temp.compareTo(startCalendar) < 0)
                         continue;
 
-                    //ok
                     commands.add(dateFormat.format(temp.getTime()) + " " + command);
                 }
+
             }
 
             //将时间改为00:00
@@ -216,6 +186,35 @@ public class Y17M12P3 {
         return values;
     }
 
+    public static ArrayList<Integer> genValues(String s, int min, int max, String tag) {
+        if (tag.equals("month")) {
+            s = s.replace("Jan", "1");
+            s = s.replace("Feb", "2");
+            s = s.replace("Mar", "3");
+            s = s.replace("Apr", "4");
+            s = s.replace("May", "5");
+            s = s.replace("Jun", "6");
+            s = s.replace("Jul", "7");
+            s = s.replace("Aug", "8");
+            s = s.replace("Sep", "9");
+            s = s.replace("Oct", "10");
+            s = s.replace("Nov", "11");
+            s = s.replace("Dec", "12");
+        }
+
+        if (tag.equals("week")) {
+            s = s.replace("Sun","0");
+            s = s.replace("Mon", "1");
+            s = s.replace("Tue", "2");
+            s = s.replace("Wed", "3");
+            s = s.replace("Thu", "4");
+            s = s.replace("Fri", "5");
+            s = s.replace("Sat", "6");
+        }
+
+        return genValues(s, min, max);
+    }
+
     public static int parseFromTag(String s, int position) {
         char tag = s.charAt(position);
         return (tag == '0') ?
@@ -224,4 +223,6 @@ public class Y17M12P3 {
     }
 }
 
-//28 min + 30 min + 30 min + 30 min + 30 min + 30 min
+// Mar 5: 28 min
+// Mar 6: 30 min + 30 min + 30 min + 30 min + 30 min
+// Mar 9: 60 min + 25 min
