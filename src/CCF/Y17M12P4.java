d9d978199e90f28e35689bf51206186a94dad0e9
snow <xueyao_98@foxmail.com>
2018-03-11 22:17:03 +0800

    CCF-P4 need to debug

--- src/CCF/Y17M12P4.java
+++ src/CCF/Y17M12P4.java
@@ -1,6 +1,7 @@
 package CCF;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Scanner;
 
 /**
@@ -39,48 +40,95 @@ public class Y17M12P4 {
             }
         }
 
-        //test
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j < n; j++) {
-//                System.out.printf(distance[i][j]+"\t");
-//            }
-//            System.out.printf("\n");
-//        }
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j < n; j++) {
-//                System.out.printf(type[i][j]+"\t");
-//            }
-//            System.out.printf("\n");
-//        }
+        //标记数组：记录所有已经遍历过的数组
+        ArrayList<Integer> tags = new ArrayList<>();
+        //每个tags中的元素，都对应着一条当前路径
+        Hashtable<Integer, Path> pathOfTag = new Hashtable<>();
 
-        ArrayList<Integer> tags = new ArrayList<>();     //标记数组：记录所有已经遍历过的数组
         tags.add(1);
 
-        ArrayList<Integer> paths = new ArrayList<>();
-        while (tags.size() < n) {
+        int choicedTag = 1;     //此次迭代所选择的路口
+        int next = -1;       //此次迭代所选择路口的下一个路口
+        int minCost = -1;        //此次迭代的最小疲劳度
+
+        while (!tags.contains(n)) {
             //遍历tag数组，对所有可达的路口编号（且不再tag中），进行路径的计算
             for (int i = 0; i < tags.size(); i++) {
-                int tempNum = tags.get(i);
+                int tagNum = tags.get(i) - 1;
                 for (int j = 0; j < n; j++) {
-                    if (distance[tempNum][j] != -1 && !tags.contains(j)) {
+                    int d = distance[tagNum][j];
+                    if (d != -1 && !tags.contains(j + 1)) {
+                        boolean t = type[tagNum][j];
+                        //first time
+                        if (next == -1) {
+                            next = j;
+                            minCost = (t == false) ? d : (d * d);
+
+                            pathOfTag.put(choicedTag + 1,
+                                    new Path(t, d, minCost, null));
+                            continue;
+                        }
+
+                        //TODO: debug cost = 29 计算函数存在问题
+                        int cost = calCost(pathOfTag.get(choicedTag + 1), t, d);
+                        if (cost < minCost) {
+                            choicedTag = tagNum;
+                            next = j;
+                            minCost = cost;
+                        }
 
                     }
                 }
+
             }
+
+            tags.add(next + 1);
+            boolean t = type[choicedTag][next];
+            int d = distance[choicedTag][next];
+            pathOfTag.put(next + 1, new Path(t, d, minCost, pathOfTag.get(choicedTag + 1)));
         }
 
+        System.out.println(pathOfTag.get(n).cost);
+
+    }
+
+    //记录道路的type: true = small, false = big
+    public static int calCost(Path path, boolean typeOfD, int d) {
+        if (path != null) {
+            //新加的是大道，则无需遍历之前
+            if (typeOfD == false) {
+                return path.cost + d;
+            }
+
+            //新加的是小道，但之前一段为大道
+            if (path.type == false) {
+                return path.cost + d * d;
+            }
+
+            //新加的是小道，之前一段也是小道
+            return calCost(path.prePath, path.type, d + path.finalDistance);
+        }
+
+        //如果遍历到第一个路口
+        if (typeOfD == false) {
+            return d;
+        }
+        return d * d;
     }
 }
 
 class Path {
-    int currNum;        //当前路径的最末端路口编号
-    int preNum;     //上一个路口编号
+    boolean type;
+    int finalDistance;
     int cost;       //疲劳度
+    Path prePath;
 
-    Path(int c, int p) {
-        currNum = c;
-        preNum = p;
+    public Path(boolean type, int finalDistance, int cost, Path prePath) {
+        this.type = type;
+        this.finalDistance = finalDistance;
+        this.cost = cost;
+        this.prePath = prePath;
     }
 }
 
-//Mar 11: 60min
+//Mar 11: 60min + 10min + 30min + 30min
